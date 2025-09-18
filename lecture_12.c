de01cf27c21da6bda72854d6133929b879e922a5
Alsifa jahan <alsifajahan4@gmail.com>
2025-09-18 14:06:01 +0530

    Add files via upload

---
+++ lecture_12.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+// int main(){
+//     int arr[5] = {1,2,3,4,5};
+//     int i;
+
+//     for(i = 0; i<5; i++){
+//         printf("%d",arr[i]);
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int arr[5] = {1,2,4};
+//     for(int i=0; i<3; i++){
+//         printf("%d",arr[i]);
+//     }
+// }
+
+// int main(){
+//     int arr[5] = {1,2,4};
+//     int max= arr[0];
+//     for(int i=1; i<3; i++){
+//         if(arr[i] > max){
+//             max = arr[i];
+
+//         }
+//     }
+//     printf("%d",max);
+//     return 0;
+// }
+
+// main(){
+//     int arr[5] = {1,2,4};
+//     int min = arr[0];
+//     for(int i=1; i<3; i++){
+//         if(arr[i] < min){
+//             min = arr[i];
+//         } 
+//     }
+//     printf("%d",min);
+//     return 0;
+// }
+
+// int main(){
+//     int arr[5] = {1,2,3};
+//     int max = arr[0];
+//     for(int i=1; i<3; i++){
+//         if(arr[i] < max){
+//         max = arr[i];
+//     }
+// }
+// printf("%d",max);
+// return 0;
+// }
+
+int main(){
+    int arr[5] = {1,2,3};
+    int max = arr[0];
+    for(int i=0; i<3;i++){
+        
+    }
+}
