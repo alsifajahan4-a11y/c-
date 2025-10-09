a59fc9ca90e616e0c0eb5edf802ab6f7decc6d0d
Alsifa jahan <alsifajahan4@gmail.com>
2025-10-09 13:51:25 +0530

    Add files via upload

---
+++ lecture23.c
@@ -0,0 +1,55 @@
+ #include<stdio.h>
+ #include<string.h>
+
+// int main(){
+//     char str[] = "hello";
+//     char target = 'e';
+//     char nstr[100];
+//     int j=0;
+//     int ln = strlen(str);
+//     for(int i=0; i<ln; i++)
+//     {
+//         if (str[i]!=target)
+//         {
+//            nstr[j]=str[i];
+//            j++;
+//         }
+        
+//     }
+//       nstr[j] = '\0';
+//         printf("%s",nstr);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// #include<string.h>
+
+// int main(){
+// char str[] = "abcabd";
+//     int ln = strlen(str);
+//     int freq[256] = {0};
+//     //  int ln = strlen(str);
+//     for (int i = 0; i < ln; i++)
+//     {
+//         freq[str[i]]++;
+//     }
+//     for (int i = 0; i < ln; i++)
+//     {
+//         if (freq[str[i]] == 1)
+//         {
+//             printf("%c", str[i]);
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+int main(){
+    char str[] = "hello";
+    char str[] = "hii";
+    int ln = strlen(str);
+    for(int i =0; i<in; i++)
+}
+
